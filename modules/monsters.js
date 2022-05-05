ae69ff14c995a94778b29cea251fc7a135229845
PrestonLeeC <preston.lee.cole@gmail.com>
2022-05-04 20:25:40 -0500

    New monster names and favicon
    Added new monster names to modules\monsters.js and an app icon with transparency, plus conversions of that icon to favicon.ico and other formats.

--- modules/monsters.js
+++ modules/monsters.js
@@ -1,101 +1,146 @@
 export const Monsters = {
-  Raptaves: {
-  // == (Hunting birds are known as raptors and birds are "aves")
-    name: "Raptaves",
-    combat_power: 0.2,
-    icon_url:"images/rathalos.png",
-    body: {
-  		impact: 3, slashing: 3, shot: 3,
-      fire: 3, water: 3,
-      shock: 0, pitfall: 0,
-  		loot: [
-  			{ name:"Jagras Scale", quantity: 60 },
-  			{ name:"Jagras Hide",  quantity: 20 },
-  		]
-  	},
-  	capture: { loot: [] }
-  },  
-  // ==============
-  Salire: {
-  // === (Means To Jump)
-    name: "Salire",
-    combat_power: 0.2,
-    icon_url:"images/rathalos.png",
-    body: {
-  		impact: 3, slashing: 3, shot: 3,
-      fire: 3, water: 3,
-      shock: 0, pitfall: 0,
-  		loot: [
-  			{ name:"Jagras Scale", quantity: 60 },
-  			{ name:"Jagras Hide",  quantity: 20 },
-  		]
-  	},
-  	capture: { loot: [] }
-  },
-  // ==============
-  Anilios: {
-  // ===== (Means Gray, but also a genus of snakes)
-    name: "Anilios",
-    combat_power: 1,
-    icon_url:"images/rathalos.png",
-    body: {
-  		impact: 3, slashing: 2, shot: 3,
-      fire: 3, water: 3,
-      shock: 2, pitfall: 3,
-  		loot: [
-  			{ name:"Great Jagras Scale", quantity: 60 },
-  			{ name:"Great Jagras Hide",  quantity: 20 },
-  			{ name:"Bird Wyvern Gem",    quantity:  1 }
-  		]
-  	},
-  	capture: {
-  		loot: [
-  			{ name:"Great Jagras Scale", quantity: 1 },
-  			{ name:"Great Jagras Hide",  quantity: 2 },
-  			{ name:"Great Jagras Mane",  quantity: 1 },
-  			{ name:"Great Jagras Claw",  quantity: 2 }
-  		]
-  	},
-  	head: {
-  		impact: 3, slashing: 2, shot: 3,
-      cut: false,
-  		loot: [
-  			{ name:"Great Jagras Mane", quantity: 2 },
-  			{ name:"Great Jagras Hide", quantity: 1 }
-  		]
-  	},
-  	stomach: {
-  		impact: 3, slashing: 2, shot: 3,
-      cut: false,
-  		loot: [
-    		{ name:"Great Jagras Scale", quantity: 1 },
-    		{ name:"Great Jagras Hide",  quantity: 3 },
-    	]
-    },
-  	left_leg: {
-  		impact: 3, slashing: 2, shot: 3,
-      cut: false,
-  		loot: [
-  			{ name:"Great Jagras Claw", quantity: 1 }
-  		]
-  	}
-  },
-  // ==============
-  Arctocine: {
-  // ====(Arctocine means grey bear)
-  // ====(Carnivoran means mammal that has bladelike carnassial teeth and eats meat)
-    name: "Arctocine",
-    combat_power: 2,
-    icon_url:"images/rathalos.png",
-    body: {
-  		impact: 3, slashing: 3, shot: 3,
-      fire: 3, water: 3,
-      shock: 0, pitfall: 0,
-  		loot: [
-  			{ name:"Jagras Scale", quantity: 60 },
-  			{ name:"Jagras Hide",  quantity: 20 },
-  		]
-  	},
-  	capture: { loot: [] }
-  },
+	// ==============
+	Marinmoth: {
+
+	},
+	// ==============
+	// Balverine inspired, but more pig than wolf.
+	// Paw Pig Wolf
+	Palvig: {
+
+	},
+	// ==============
+	// Unicorn? Harlequin-like techniques?
+	Hartleigh: {
+
+	},
+	// ==============
+	Abnocte: {
+
+	},
+	// ==============
+	// Ygsfalo (Ex- Ygg is a tree, important to all that is holy)
+	// Gold Rot Fright Viper Prime Eval Azid
+	// Bug Pest Cloud Hound Figure Dweller
+	// Goulof (Colossal Golem Gulag)
+	// Stormwile (Storm Wild)
+	// Saphraze (Sapphire Razor)
+	// Teraverba (Terrafirma Herbivore)
+	// Gandell (Candle) (forge fire weapon: Gandell Wick)
+	// ==============
+	// Fearan (Fear Talon Raptor) & Fearan Beth
+	// Beth (Pest) as suffix for "Great" or Tempered (Heroic version)
+	Fearan: {
+
+	},
+	// ==============
+	// Zeser (Blazeserpent) & Zeser Beth
+	Zeser: {
+
+	},
+	// ==============
+	// Vigure Beth (Viper Figure Pest)
+	Vigure: {
+
+	},
+	// ==============
+	// Zeser (Blazeserpent) & Zeser Beth
+	Zeser: {
+			name: "Zeser",
+			combat_power: 0.2,
+			icon_url:"monsters/Zeser.png",
+			body: {
+			impact: 3, slashing: 3, shot: 3,
+					fire: 3, water: 3,
+					shock: 0, pitfall: 0,
+			loot: [
+				{ name:"Zeser Scale", quantity: 60 },
+				{ name:"Zeser Hide",  quantity: 20 },
+				{ name:"Zeser Extract",  quantity: 20 },
+				{ name:"Zeser Jawbone",  quantity: 20 },
+			]
+		},
+		capture: { loot: [] }
+	},  
+	// ==============
+	Saelvire: {
+			name: "Saelvire",
+			combat_power: 0.2,
+			icon_url:"monsters/Saelvire.png",
+			body: {
+			impact: 3, slashing: 3, shot: 3,
+					fire: 3, water: 3,
+					shock: 0, pitfall: 0,
+			loot: [
+				{ name:"Saelvire Scale", quantity: 60 },
+				{ name:"Saelvire Hide",  quantity: 20 },
+			]
+		},
+		capture: { loot: [] }
+	},
+	// ==============
+	Venilios: {
+			name: "Venilios",
+			combat_power: 1,
+			icon_url:"monsters/Venilios.png",
+			body: {
+			impact: 3, slashing: 2, shot: 3,
+					fire: 3, water: 3,
+					shock: 2, pitfall: 3,
+			loot: [
+				{ name:"Venilios Scale", quantity: 60 },
+				{ name:"Venilios Hide",  quantity: 20 },
+				{ name:"Venilios Plate",    quantity:  1 }
+			]
+		},
+		capture: {
+			loot: [
+				{ name:"Venilios Scale", quantity: 1 },
+				{ name:"Venilios Hide",  quantity: 2 },
+				{ name:"Venilios Mane",  quantity: 1 },
+				{ name:"Venilios Claw",  quantity: 2 }
+			]
+		},
+		head: {
+			impact: 3, slashing: 2, shot: 3,
+					cut: false,
+			loot: [
+				{ name:"Venilios Mane", quantity: 2 },
+				{ name:"Venilios Hide", quantity: 1 }
+			]
+		},
+		stomach: {
+			impact: 3, slashing: 2, shot: 3,
+					cut: false,
+			loot: [
+					{ name:"Venilios Scale", quantity: 1 },
+					{ name:"Venilios Hide",  quantity: 3 },
+				]
+			},
+		left_leg: {
+			impact: 3, slashing: 2, shot: 3,
+					cut: false,
+			loot: [
+				{ name:"Venilios Claw", quantity: 1 }
+			]
+		}
+	},
+	// ==============
+	// ====(Arctocine means grey bear)
+	// ====(Carnivoran means mammal that has bladelike carnassial teeth and eats meat)
+	Arctocine: {
+			name: "Arctocine",
+			combat_power: 2,
+			icon_url:"monsters/rathalos.png",
+			body: {
+			impact: 3, slashing: 3, shot: 3,
+					fire: 3, water: 3,
+					shock: 0, pitfall: 0,
+			loot: [
+				{ name:"Arctocine Scale", quantity: 60 },
+				{ name:"Arctocine Hide",  quantity: 20 },
+			]
+		},
+		capture: { loot: [] }
+	},
 }
